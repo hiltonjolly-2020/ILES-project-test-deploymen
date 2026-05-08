1db0921acd7c620699d476ce05917339434c0b28
hiltonjolly-2020 <hiltonjolly@gmail.com>
2026-05-07 18:32:05 -0700

    Fix auto_setup location in management/commands folder

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn ILES.wsgi:application 
---
+++ build.bat
@@ -0,0 +1,11 @@
+@echo off 
+echo Installing backend dependencies... 
+pip install -r requirements.txt 
+ 
+echo Collecting static files... 
+python manage.py collectstatic --noinput 
+ 
+echo Running migrations... 
+python manage.py migrate 
+ 
+echo Build completed! 
